aa95845e5bc18a76befee262e40254475ffdb619
Arjun-M2003 <112738672+Arjun-M2003@users.noreply.github.com>
2025-02-11 12:34:00 -0500

    initial page setup, gonna add some styles and stuff for homepage

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home'
+import Account from './pages/Account'
+import NoPage from './pages/NoPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          Grubgram
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route index element= {<Home />} />
+        <Route path = "/home" element={<Home />} />
+        <Route path = "/account" element={<Account />} />
+        <Route path = "*" element={<NoPage />} />
+      </Routes>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/pages/Account.js
@@ -0,0 +1,10 @@
+import Header from "../components/Header";
+
+export default function(){
+    return (
+        <>
+            <Header />
+            <h2>Account</h2>
+        </>
+    )
+}
