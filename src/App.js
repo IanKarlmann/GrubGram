0f01deb643d72ea1aa06c3450a2b0a4a6f305e2f
Arjun-M2003 <112738672+Arjun-M2003@users.noreply.github.com>
2025-02-19 19:35:24 -0500

    another update for nopage

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route, Navigate, useNavigate } from "react-route
 import { useEffect, useState } from "react";
 import Home from "./pages/Home";
 import Account from "./pages/Account";
-import NoPage from "./pages/NoPage";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
