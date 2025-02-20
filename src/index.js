de17a4fb22c5160c4b6e72743f186b36af35434e
Arjun-M2003 <112738672+Arjun-M2003@users.noreply.github.com>
2025-02-19 19:25:02 -0500

    added a comment so I can make a comment and test ci

--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@ root.render(
   </React.StrictMode>
 );
 
+// adding a comment so I can make a commit and test ci
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
