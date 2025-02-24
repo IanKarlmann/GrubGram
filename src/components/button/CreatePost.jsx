821d061e5b89bae6fc9eac4f1f1b68cacb18d7b8
Arjun-M2003 <112738672+Arjun-M2003@users.noreply.github.com>
2025-02-23 22:13:35 -0500

    post stuff

---
+++ src/components/button/CreatePost.jsx
@@ -0,0 +1,13 @@
+import PropTypes from 'prop-types';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import { createTheme } from '@mui/material/styles';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import PersonIcon from '@mui/icons-material/Person';
+import BarChartIcon from '@mui/icons-material/BarChart';
+import DescriptionIcon from '@mui/icons-material/Description';
+import { AppProvider } from '@toolpad/core/AppProvider';
+import { DashboardLayout } from '@toolpad/core/DashboardLayout';
+import { useDemoRouter } from '@toolpad/core/internal';
+
+
