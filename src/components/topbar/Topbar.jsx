3cb6f2a5bd063a1d77e0386654a090a30ed96186
Arjun-M2003 <112738672+Arjun-M2003@users.noreply.github.com>
2025-04-17 17:04:14 -0400

    ui updates

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -8,9 +8,6 @@ export default function Topbar({ toggleForm }) {
                 <span className="logo">GrubGram</span>
             </div>
             <div className="topbarRight">
-                <div className="topbarLinks">
-                    <span className="topbarLink">Homepage</span>
-                </div>
                 <button onClick={toggleForm} className="createPostButton">
                     Create Post
                 </button>
